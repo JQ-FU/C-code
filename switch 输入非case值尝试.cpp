0f718526595da108bf25bf50ecdb0fb266f450fa
JQ-FU <255073593@qq.com>
2021-03-30 10:40:11 +0800

    我

---
+++ switch 输入非case值尝试.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int c;
+	scanf("%d",&c);
+	switch(c)
+	{
+		case 1:
+			printf("1");
+			break;
+			
+		case 2:
+			printf("2");
+			break;
+			
+			
+	}
+	return 0;
+	
+}
